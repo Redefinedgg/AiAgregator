fcf1b08a9ae5054e049949f2c735a1ab6ae41fdd
ensomnatt <ensomnatt@protonmail.com>
2025-09-08 15:20:35 +0300

    fix ChatItem bug

--- frontend/src/features/Sidebar/ChatItem/ui/index.tsx
+++ frontend/src/features/Sidebar/ChatItem/ui/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function ChatItem({ uuid, name }: Props) {
   return (
-    <Link key={uuid} href={`chat/${uuid}`} className="block p-2 rounded text-[20px] hover:opacity-90 hover:bg-[#11141C] p-[8px] rounded-[12px]">
+    <Link href={`/chat/${uuid}`} className="block p-2 rounded text-[20px] hover:opacity-90 hover:bg-[#11141C] p-[8px] rounded-[12px]">
       {name || "Untitled"}
     </Link>
   );
